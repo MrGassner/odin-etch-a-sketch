a450c702df1e4b422ef8d21e297454fc4b15685a
MrGassner <110873341+MrGassner@users.noreply.github.com>
2023-01-24 09:45:07 -0300

    Add multiple board sizes functionality

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-const game = document.getElementById('game')
-let gameSize = 16
+// game board
+const game = document.getElementById('game');
+let boardSize = document.querySelectorAll('.sizeBtns')
+let defaultSize = 16
 
-
-
-for (let i = 0; i < (gameSize * gameSize); i++) {
-
-    const gameGrid = document.createElement('div');
-    gameGrid.id = 'gameGrid'
-    game.appendChild(gameGrid);
-}
-
-document.querySelectorAll('#gameGrid').forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.classList.add('drawn')
-    })
+document.querySelectorAll('.sizeBtns').forEach(btn => {
+    btn.addEventListener('click', event => boardResize(event))
 })
 
+function board(size) {
+
+    document.querySelector('#game').style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    for (let i = 0; i < (size * size); i++) {
+        const gameGrid = document.createElement('div');
+        gameGrid.id = 'gameGrid';
+        game.appendChild(gameGrid);
+    }
+
+    return draw()
+}
+
+function boardResize(event) {
+    while (game.firstChild) {
+        game.removeChild(game.lastChild)
+    }
+
+    let newSize = event.target.classList.value;
+    return board(newSize)
+}
+
+
+// Drawing on board
+function draw() {
+    let mouseDown = false
+    document.body.onmousedown = () => (mouseDown = true)
+    document.body.onmouseup = () => (mouseDown = false)
+
+
+    document.querySelectorAll('#gameGrid').forEach(square => {
+        square.addEventListener('mouseover', event => {
+            
+            let color = document.getElementById('colorPicker').value;
+
+            if (event.type === 'mouseover' && !mouseDown) return;
+            square.classList.add('drawn');
+            square.style.background = color;    
+        });
+    });
+}
+
+board(defaultSize)
